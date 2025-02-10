d8f272847b08bea27b3b4c9ae9c19b1f8ae8f7e6
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-02-10 18:49:13 +0100

    add something

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import mammalsRouter from './routes/mammalRouter.js';
+import mammalsRouter from './routes/mammalsRouter.js';
  const PORT = 3001;
+ 
  const app = express ();
  app.get("/",(req,res) => {
     res.send("Zoo Assignment")
  })
 
  app.use("/mammals", mammalsRouter)
+
  app.listen(PORT, () => console.log(`Listening on port : ${PORT}`));
