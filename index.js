3a2d40d8706efdcbc00c67fb4bfe1ee81d4d78d5
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-02-13 11:42:31 +0100

    add

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 
+
+ 
+
 app.get("/",(req,res) => {
     res.render("pages/home", {
       title : "Welcome to the Bronx zoo",
       bodyClass : "home"
     })
  })
+
+ app.use("/mammals",mammalsRouter);
+ 
  app.listen(PORT, () => console.log(`Listening on port : ${PORT}`));
