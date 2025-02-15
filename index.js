9cf3e51d18172eaa16b9de2420da039fb9f7eed2
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-02-15 19:39:02 +0100

    Fixing errors caused by merge conflicts

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import mammalsRouter from "./routes/mammalsRouter.js";
 import * as path from "path";
 
 const PORT = 3000;
@@ -9,7 +8,6 @@ const __dirname = path.resolve();
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
-<<<<<<< HEAD
 app.get("/",(req,res) => {
     res.render("pages/home", {
       title : "Welcome to the Bronx zoo",
@@ -18,7 +16,6 @@ app.get("/",(req,res) => {
 })
 app.use("/mammals",mammalsRouter);
 app.listen(PORT, () => console.log(`Listening on port : ${PORT}`));
-=======
 
 import allAnimals from './data/animal.js';
 import mammalsRouter from './routes/mammalRouter.js';
@@ -56,4 +53,3 @@ app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
 
->>>>>>> origin/main
