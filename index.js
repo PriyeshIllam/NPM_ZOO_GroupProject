7ed5fb1618a550c0a52eef0744dc446d244f09df
Ankitasingh88 <ankita.shibu.singh@gmail.com>
2025-02-15 10:46:53 +0100

    add footer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mammalsRouter from "./routes/mammalsRouter.js";
 import * as path from "path";
 const PORT = 3001;
 const app = express ();
@@ -7,17 +8,11 @@ const __dirname = path.resolve();
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
-
-
- 
-
 app.get("/",(req,res) => {
     res.render("pages/home", {
       title : "Welcome to the Bronx zoo",
       bodyClass : "home"
     })
- })
-
- app.use("/mammals",mammalsRouter);
- 
- app.listen(PORT, () => console.log(`Listening on port : ${PORT}`));
+})
+app.use("/mammals",mammalsRouter);
+app.listen(PORT, () => console.log(`Listening on port : ${PORT}`));
--- routes/mammalsRouter.js
+++ routes/mammalsRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+
 const mammalsRouter = express.Router();
 
 mammalsRouter.get("/",(req,res) => {
