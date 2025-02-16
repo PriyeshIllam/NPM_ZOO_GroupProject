86514f9f84d90136a3fca6002c60d7b9f333dda4
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-02-16 12:13:57 +0100

    deleting whitespaces

--- index.js
+++ index.js
@@ -12,27 +12,24 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 
-import birds from './data/animal.js'; // import the birds data
-
 app.get('/', (req, res) => {
     res.render("pages/home",{
         animals : allAnimals, //display all animals in sidebar
         content : homeContent, //maincontent
         bodyClass : "home"
-    })
-                         
+    })                   
 });
 
 app.get('/animal/:name', (req, res) => {
-        const animalName = req.params.name; //access route parameters
-        let animal;
+    const animalName = req.params.name; //access route parameters
+    let animal;
     
-        for (let i = 0; i < allAnimals.length; i++) {
-            if (allAnimals[i].name === animalName) {
-                animal = allAnimals[i];
-                break; 
-            }
+    for (let i = 0; i < allAnimals.length; i++) {
+        if (allAnimals[i].name === animalName) {
+            animal = allAnimals[i];
+            break; 
         }
+    }
 
     if (animal) {
         res.render('pages/animal.ejs', {animal});
@@ -41,13 +38,8 @@ app.get('/animal/:name', (req, res) => {
     }
 });
 
-app.get('/birds', (req, res) => {
-    const onlyBirds = allAnimals.filter(animal => animal.type === "Bird");
-    res.render('pages/birds.ejs', { birds: onlyBirds });
-});
-
-app.use("/mammals",mammalsRouter)
-app.use("/reptiles",reptilesRouter)
+app.use("/mammals",mammalsRouter);
+app.use("/reptiles",reptilesRouter);
 app.use("/birds",birdsRouter);
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
